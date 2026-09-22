e7c6f50f519dbe75b7ae7d1e370a32f444980da4
mudz <jg514williams@gmail.com>
2026-09-21 23:38:09 -0700

    Premake | Game Engine Series

---
+++ premake5.lua
@@ -0,0 +1,129 @@
+workspace "HazelClone"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "HazelClone"
+    location "HazelClone"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{MKDIR} ../bin/" .. outputdir .. "/Sandbox"),
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/")
+        }
+
+    filter "system:linux"
+        cppdialect "C++17"
+
+        defines
+        {
+            "HZ_PLATFORM_LINUX",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{MKDIR} ../bin/" .. outputdir .. "/Sandbox"),
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        symbols "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "HazelClone/vendor/spdlog/include",
+        "HazelClone/src"
+    }
+
+    links
+    {
+        "HazelClone"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS"
+        }
+
+    filter "system:linux"
+        cppdialect "C++17"
+
+        defines
+        {
+            "HZ_PLATFORM_LINUX"
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        symbols "On"
